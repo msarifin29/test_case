41b8adc371f03e043d846e8760a84bce4d37fd91
samsul <ms.arifin29@gmail.com>
2024-06-07 18:38:04 +0800

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# test_case
+
+Required Flutter 3.16 or Support dart 3.0
---
+++ android/app/src/main/kotlin/com/viapulsa/test_case/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.viapulsa.test_case
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
